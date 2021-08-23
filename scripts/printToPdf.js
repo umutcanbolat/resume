9726b05d157fac2ac7c654ea0ea2722ea5646b27
umutcanbolat <10065235+umutcanbolat@users.noreply.github.com>
2021-08-23 17:31:12 +0200

    ci: create pdf creation script

---
+++ scripts/printToPdf.js
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:5000', {
+    waitUntil: 'networkidle2',
+  });
+  await page.pdf({
+    path: 'Umut Canbolat CV.pdf',
+    format: 'a4',
+    preferCSSPageSize: true,
+  });
+
+  await browser.close();
+})();
