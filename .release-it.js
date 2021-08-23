059da489aedace2d9d41cc20b50305e6d1054320
umutcanbolat <10065235+umutcanbolat@users.noreply.github.com>
2021-08-23 17:44:30 +0200

    ci: add release-it config

---
+++ .release-it.js
@@ -0,0 +1,24 @@
+module.exports = {
+  git: {
+    commitMessage: 'chore(release): v${version}',
+    requireBranch: 'master',
+    requireCommits: 'true',
+  },
+  npm: {
+    publish: false,
+  },
+  github: {
+    release: true,
+    assets: ['output/*.pdf'],
+    releaseName: 'v${version}',
+  },
+  plugins: {
+    '@release-it/conventional-changelog': {
+      preset: 'angular',
+      infile: 'CHANGELOG.md',
+    },
+  },
+  hooks: {
+    'after:bump': 'yarn build && yarn serveAndPrint',
+  },
+};
