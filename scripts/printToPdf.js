115152f6e97fcb31b65bf206ee9ae20746b89af8
umutcanbolat <10065235+umutcanbolat@users.noreply.github.com>
2021-08-23 18:16:46 +0200

    build: use serve-handler

--- scripts/printToPdf.js
+++ scripts/printToPdf.js
@@ -1,16 +1,31 @@
+/* eslint-disable no-console */
+const http = require('http');
+const handler = require('serve-handler');
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('http://localhost:5000', {
-    waitUntil: 'networkidle2',
-  });
-  await page.pdf({
-    path: './output/Umut Canbolat CV.pdf',
-    format: 'a4',
-    preferCSSPageSize: true,
-  });
+const server = http.createServer((request, response) => {
+  return handler(request, response, { public: 'build' });
+});
 
-  await browser.close();
-})();
+server.listen(5000, () => {
+  console.log('✅ Server is running at http://localhost:5000');
+
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:5000', {
+      waitUntil: 'networkidle2',
+    });
+    await page.pdf({
+      path: './output/Umut Canbolat CV.pdf',
+      format: 'a4',
+      preferCSSPageSize: true,
+    });
+
+    console.log('✅ PDF file is created.');
+    await browser.close();
+
+    console.log('✅ Shutting down.');
+    server.close();
+  })();
+});
