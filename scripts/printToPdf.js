639c69b67fcb9d4e07406cb8e2b21d2230393b3a
umutcanbolat <10065235+umutcanbolat@users.noreply.github.com>
2021-08-23 23:57:56 +0200

    refactor: refactor toPdf script

--- scripts/printToPdf.js
+++ scripts/printToPdf.js
@@ -1,31 +1,40 @@
 /* eslint-disable no-console */
 const http = require('http');
+const fs = require('fs');
 const handler = require('serve-handler');
 const puppeteer = require('puppeteer');
+
+async function toPdf() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:5000', {
+    waitUntil: 'networkidle2',
+  });
+  await page.pdf({
+    path: './output/Umut Canbolat CV.pdf',
+    format: 'a4',
+    preferCSSPageSize: true,
+  });
+
+  console.log('✅ PDF file is created.');
+  await browser.close();
+}
 
 const server = http.createServer((request, response) => {
   return handler(request, response, { public: 'build' });
 });
 
-server.listen(5000, () => {
+server.listen(5000, async () => {
   console.log('✅ Server is running at http://localhost:5000');
 
-  (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('http://localhost:5000', {
-      waitUntil: 'networkidle2',
-    });
-    await page.pdf({
-      path: './output/Umut Canbolat CV.pdf',
-      format: 'a4',
-      preferCSSPageSize: true,
-    });
+  fs.mkdir('./output', { recursive: true }, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
 
-    console.log('✅ PDF file is created.');
-    await browser.close();
+  await toPdf();
 
-    console.log('✅ Shutting down.');
-    server.close();
-  })();
+  console.log('✅ Shutting down the server.');
+  server.close();
 });
